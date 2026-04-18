8e8e88428f32ecda4193e01816639fcb1d151d3b
and3k <andrewjkasten@gmail.com>
2026-04-18 11:20:29 -0600

    changed so a receiver or sender can be many users

--- backend/messaging/models.py
+++ backend/messaging/models.py
@@ -3,8 +3,8 @@ from users.models import User
 # Create your models here.
 
 class Messages(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owner_profile')
-    walker = models.OneToOneField(User, on_delete=models.CASCADE, related_name='walker_profile')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_message')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_message')
     message = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
   

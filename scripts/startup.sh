08503459bf9fe60884df8c3fb1e39ad1e4c557fc
brothersd <brothersd@hotmail.com>
2026-04-18 12:36:21 -0600

    update cleanup.sh and startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,2 @@
-# Create and activate a virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-# Install the necessary library
-pip install -r requirements.txt
+#!/bin/bash
+docker compose up -d --build

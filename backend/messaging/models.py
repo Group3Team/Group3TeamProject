8e134cb7ed96e35482b97162511076a9720a826d
and3k <andrewjkasten@gmail.com>
2026-04-18 10:45:31 -0600

    model and serialiazer

--- backend/messaging/models.py
+++ backend/messaging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+class Messages(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owner_profile')
+    walker = models.OneToOneField(User, on_delete=models.CASCADE, related_name='walker_profile')
+    message = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+  

abe14b1450665d33ebae41dfcd11c9124a317dc4
Brian Perry <perry.brian@gmail.com>
2026-04-18 15:57:06 -0400

    Fix messaging app bugs: Rename model to Message, fix related names, update serializers and views

--- backend/messaging/models.py
+++ backend/messaging/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from users.models import User
 # Create your models here.
 
-class Messages(models.Model):
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_message')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_message')
+class Message(models.Model):
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     message = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
-  

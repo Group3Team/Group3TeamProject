444af9c6a2dd15e2d91d7024eff32d5d9fdae5ec
and3k <andrewjkasten@gmail.com>
2026-04-18 11:44:55 -0600

    Created view and url for get requests

--- backend/messaging/views.py
+++ backend/messaging/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Messages
+from .serializers import MessagesSerializer
 
 # Create your views here.
+class MessagesView():
+
+    def get(self, request):
+        messages = Messages.objects.all()
+        serializer = MessagesSerializer(messages, many=True)
+        return Response(serializer.data)

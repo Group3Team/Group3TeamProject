8de1f1c8d15bc3566e78e4184ff7a4532ea39aa3
brothersd <brothersd@hotmail.com>
2026-04-25 12:10:27 -0600

    update /scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,12 +2,17 @@
 
 # 1. Docker Clean Up
 # Prunes unused containers, networks, and images to free up space.
-docker system prune -f
+#docker system prune -f
 
 # 2. Build and Start Containers
 docker compose up -d --build
 
+sleep 5
 # 3. Database Migrations
 docker compose exec backend python manage.py makemigrations
 docker compose exec backend python manage.py migrate
+echo ""
+# Print warning in RED
+echo -e "\033[0;31m⚠️  If you see 'Error: That username is already taken', press CTRL-C to exit script.\033[0m"
+echo ""
 docker compose exec backend python manage.py createsuperuser

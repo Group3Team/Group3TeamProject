45d24f642900f8bee021b29ef9594d77ed021e21
and3k <andrewjkasten@gmail.com>
2026-05-05 20:22:19 -0600

    display user info and conditional  render for owner/walker

--- frontend/src/pages/ProfileOwner.jsx
+++ frontend/src/pages/ProfileOwner.jsx
@@ -1,4 +1,12 @@
+import { useAuth } from '../context/AuthContext';
+
+
 export default function ProfileOwner() {
+    const { user } = useAuth();
+
+    console.log(user)
+    
+
   return (
     <div className="profile-page">
       <h2>Profile</h2>
@@ -8,14 +16,15 @@ export default function ProfileOwner() {
         <div className="profile-section">
           <img src="" alt="" className="profile-avatar" />
           <div className="profile-section-body">
-            <p className="profile-label">Name</p>
+            <p className="profile-label">{user.username}</p>
             <p className="profile-value"></p>
-            <p className="profile-label">Address</p>
+            <p className="profile-label">{user.email}</p>
             <p className="profile-value"></p>
           </div>
         </div>
 
         {/* Dog */}
+        {user?.role === 'OWNER' ? (
         <div className="profile-section">
           <img src="" alt="" className="profile-thumb" />
           <div className="profile-section-body">
@@ -23,12 +32,13 @@ export default function ProfileOwner() {
             <p className="profile-value"></p>
           </div>
         </div>
+        ) : (<></>)}
 
         {/* Settings */}
         <div className="profile-section profile-settings">
           <div className="profile-section-body">
             <h3>Settings</h3>
-            <p className="profile-label">Username</p>
+            <p className="profile-label">Username: {user.username} </p>
             <p className="profile-value"></p>
           </div>
         </div>
